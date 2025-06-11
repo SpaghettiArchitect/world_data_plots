cf70033263b181f76d14533be873c1508a55d8a8
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-10 20:37:26 -0600

    Refactor script organization
    - Created a main funtion to the script so is easy to recognize that it
    can be run.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -3,33 +3,40 @@ from pathlib import Path
 
 import plotly.express as px
 
-# Read data as a string and convert to a Python object.
-path = Path(r"eq_data\eq_data_30_day_m1.geojson.json")
-contents = path.read_text("utf8")
-all_eq_data = json.loads(contents)
 
-# Examine all earthquakes in the dataset.
-all_eq_dicts = all_eq_data["features"]
+def main() -> None:
+    """The main function of the script."""
+    # Read data as a string and convert to a Python object.
+    path = Path(r"eq_data\eq_data_30_day_m1.geojson.json")
+    contents = path.read_text("utf8")
+    all_eq_data = json.loads(contents)
 
-# Store the magnitude, longitude, latitude and title of each earthquake.
-magnitudes, longitudes, latitudes, eq_titles = [], [], [], []
-for eq_dict in all_eq_dicts:
-    magnitudes.append(eq_dict["properties"]["mag"])
-    longitudes.append(eq_dict["geometry"]["coordinates"][0])
-    latitudes.append(eq_dict["geometry"]["coordinates"][1])
-    eq_titles.append(eq_dict["properties"]["title"])
+    # Examine all earthquakes in the dataset.
+    all_eq_dicts = all_eq_data["features"]
 
-# Build a simple world map that displays the location of earthquakes.
-title = "Global Earthquakes"
-fig = px.scatter_geo(
-    lat=latitudes,
-    lon=longitudes,
-    size=magnitudes,
-    title=title,
-    color=magnitudes,
-    color_continuous_scale="temps",
-    labels={"color": "Magnitude"},
-    projection="natural earth",
-    hover_name=eq_titles,
-)
-fig.show()
+    # Store the magnitude, longitude, latitude and title of each earthquake.
+    magnitudes, longitudes, latitudes, eq_titles = [], [], [], []
+    for eq_dict in all_eq_dicts:
+        magnitudes.append(eq_dict["properties"]["mag"])
+        longitudes.append(eq_dict["geometry"]["coordinates"][0])
+        latitudes.append(eq_dict["geometry"]["coordinates"][1])
+        eq_titles.append(eq_dict["properties"]["title"])
+
+    # Build a simple world map that displays the location of earthquakes.
+    title = "Global Earthquakes"
+    fig = px.scatter_geo(
+        lat=latitudes,
+        lon=longitudes,
+        size=magnitudes,
+        title=title,
+        color=magnitudes,
+        color_continuous_scale="temps",
+        labels={"color": "Magnitude"},
+        projection="natural earth",
+        hover_name=eq_titles,
+    )
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
